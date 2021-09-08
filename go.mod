7448e249eaf8232cede8598bc456fbbddb4c9aa9
Taylor Bartlett <taybart@gmail.com>
2021-09-08 17:34:01 -0600

    fix: working processor

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/taybart/brainfuck
 
 go 1.17
+
+require github.com/taybart/log v1.5.1
--- main.go
+++ main.go
@@ -1,55 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// char array[30000] = {0}; char *ptr = array;
-// >	++ptr;
-// <	--ptr;
-// +	++*ptr;
-// -	--*ptr;
-// .	putchar(*ptr);
-// ,	*ptr = getchar();
-// [	while (*ptr) {
-// ]	}
+	"github.com/taybart/log"
+)
+
+const (
+	DONE = 0
+)
 
 type fucked struct {
+	code      []rune
 	stack     []byte
 	ptr       int
 	looplines []int
 }
 
+var code = `
++++++ +++++             initialize counter (cell #0) to 10
+[                       use loop to set 70/100/30/10
+    > +++++ ++              add  7 to cell #1
+    > +++++ +++++           add 10 to cell #2
+    > +++                   add  3 to cell #3
+    > +                     add  1 to cell #4
+<<<< -                  decrement counter (cell #0)
+]
+> ++ .                  print 'H'
+> + .                   print 'e'
++++++ ++ .              print 'l'
+.                       print 'l'
++++ .                   print 'o'
+> ++ .                  print ' '
+<< +++++ +++++ +++++ .  print 'W'
+> .                     print 'o'
++++ .                   print 'r'
+----- - .               print 'l'
+----- --- .             print 'd'
+> + .                   print '!'
+> .                     print '\n'
+`
+
 func main() {
-	code := []rune{'+', '+', '>', '+'}
+	// log.SetLevel(log.DEBUG)
 	b := fucked{
 		stack:     make([]byte, 20),
 		ptr:       0,
 		looplines: []int{},
 	}
-	for _, op := range code {
-		b.process(op)
-	}
+	b.process(code)
 	fmt.Printf("%+v\n", b.stack)
 }
 
-func (b *fucked) process(in rune) int {
-	switch in {
-	case '<':
-		b.ptr--
-	case '>':
-		b.ptr++
-	case '+':
-		b.stack[b.ptr]++
-	case '-':
-		b.stack[b.ptr]--
-	case '[':
-		b.looplines = append(b.looplines, b.ptr)
-	case ']':
-		if b.stack[b.ptr] != 0 {
-			b.ptr = b.looplines[len(b.looplines)-1]
-			return b.ptr
+func (b *fucked) process(code string) {
+	i := 0
+	for i != len(code) {
+		op := code[i]
+		switch op {
+		case '<':
+			log.Debugf("sub pointer %d -> %d\n", b.ptr, b.ptr-1)
+			b.ptr--
+		case '>':
+			log.Debugf("add pointer %d -> %d\n", b.ptr, b.ptr+1)
+			b.ptr++
+		case '+':
+			v := b.stack[b.ptr]
+			log.Debugf("add [%d] %d -> %d\n", b.ptr, v, v+1)
+			b.stack[b.ptr] = v + 1
+		case '-':
+			v := b.stack[b.ptr]
+			log.Debugf("sub [%d] %d -> %d\n", b.ptr, v, v-1)
+			b.stack[b.ptr] = v - 1
+		case '[':
+			// take note of location in code to jump back to
+			log.Debug("loop start")
+			b.looplines = append(b.looplines, i)
+		case ']':
+			if b.stack[b.ptr] == DONE {
+				log.Debug("loop done")
+				b.looplines = b.looplines[:len(b.looplines)-1]
+			} else {
+				i = b.looplines[len(b.looplines)-1]
+				log.Debugf("loop to %d\n", i)
+				continue
+			}
+		case '.':
+			fmt.Print(string(b.stack[b.ptr]))
 		}
-	case '.':
-		fmt.Println(b.stack[b.ptr])
+		i++
 	}
-	return -1
 }
