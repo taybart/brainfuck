63a29ef074a18be0d0dcb6e277966d0ddfc71730
Taylor Bartlett <taybart@gmail.com>
2021-09-08 18:58:47 -0600

    feat: add clean function to speed up runs

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"github.com/taybart/log"
 )
@@ -11,57 +13,90 @@ const (
 )
 
 type fucked struct {
-	code      []rune
-	stack     []byte
-	ptr       int
-	looplines []int
+	code  []rune
+	stack []byte
+	ptr   int
+	loops []int
 }
 
+// var code = `
+// +++++ +++++             initialize counter (cell #0) to 10
+// [                       use loop to set 70/100/30/10
+//     > +++++ ++              add  7 to cell #1
+//     > +++++ +++++           add 10 to cell #2
+//     > +++                   add  3 to cell #3
+//     > +                     add  1 to cell #4
+// <<<< -                  decrement counter (cell #0)
+// ]
+// > ++ .                  print 'H'
+// > + .                   print 'e'
+// +++++ ++ .              print 'l'
+// .                       print 'l'
+// +++ .                   print 'o'
+// > ++ .                  print ' '
+// << +++++ +++++ +++++ .  print 'W'
+// > .                     print 'o'
+// +++ .                   print 'r'
+// ----- - .               print 'l'
+// ----- --- .             print 'd'
+// > + .                   print '!'
+// > .                     print '\n'
+// `
+
 var code = `
-+++++ +++++             initialize counter (cell #0) to 10
-[                       use loop to set 70/100/30/10
-    > +++++ ++              add  7 to cell #1
-    > +++++ +++++           add 10 to cell #2
-    > +++                   add  3 to cell #3
-    > +                     add  1 to cell #4
-<<<< -                  decrement counter (cell #0)
-]
-> ++ .                  print 'H'
-> + .                   print 'e'
-+++++ ++ .              print 'l'
-.                       print 'l'
-+++ .                   print 'o'
-> ++ .                  print ' '
-<< +++++ +++++ +++++ .  print 'W'
-> .                     print 'o'
-+++ .                   print 'r'
------ - .               print 'l'
------ --- .             print 'd'
-> + .                   print '!'
-> .                     print '\n'
+-,+[                         Read first character and start outer character reading loop
+    -[                       Skip forward if character is 0
+        >>++++[>++++++++<-]  Set up divisor (32) for division loop
+                               (MEMORY LAYOUT: dividend copy remainder divisor quotient zero zero)
+        <+<-[                Set up dividend (x minus 1) and enter division loop
+            >+>+>-[>>>]      Increase copy and remainder / reduce divisor / Normal case: skip forward
+            <[[>+<-]>>+>]    Special case: move remainder back to divisor and increase quotient
+            <<<<<-           Decrement dividend
+        ]                    End division loop
+    ]>>>[-]+                 End skip loop; zero former divisor and reuse space for a flag
+    >--[-[<->+++[-]]]<[         Zero that flag unless quotient was 2 or 3; zero quotient; check flag
+        ++++++++++++<[       If flag then set up divisor (13) for second division loop
+                               (MEMORY LAYOUT: zero copy dividend divisor remainder quotient zero zero)
+            >-[>+>>]         Reduce divisor; Normal case: increase remainder
+            >[+[<+>-]>+>>]   Special case: increase remainder / move it back to divisor / increase quotient
+            <<<<<-           Decrease dividend
+        ]                    End division loop
+        >>[<+>-]             Add remainder back to divisor to get a useful 13
+        >[                   Skip forward if quotient was 0
+            -[               Decrement quotient and skip forward if quotient was 1
+                -<<[-]>>     Zero quotient and divisor if quotient was 2
+            ]<<[<<->>-]>>    Zero divisor and subtract 13 from copy if quotient was 1
+        ]<<[<<+>>-]          Zero divisor and add 13 to copy if quotient was 0
+    ]                        End outer skip loop (jump to here if ((character minus 1)/32) was not 2 or 3)
+    <[-]                     Clear remainder from first division if second division was skipped
+    <.[-]                    Output ROT13ed character from copy and clear it
+    <-,+                     Read next character
+]                            End character reading loop
 `
 
 func main() {
 	// log.SetLevel(log.DEBUG)
 	b := fucked{
-		stack:     make([]byte, 20),
-		ptr:       0,
-		looplines: []int{},
+		stack: make([]byte, 20),
+		ptr:   0,
+		loops: []int{},
 	}
 	b.process(code)
-	fmt.Printf("%+v\n", b.stack)
+	// fmt.Printf("%+v\n", b.stack)
 }
 
 func (b *fucked) process(code string) {
+	cleaned := b.clean(code)
 	i := 0
-	for i != len(code) {
-		op := code[i]
+	for i != len(cleaned) {
+		fmt.Println(b.String())
+		op := cleaned[i]
 		switch op {
 		case '<':
-			log.Debugf("sub pointer %d -> %d\n", b.ptr, b.ptr-1)
+			// log.Debugf("sub pointer %d -> %d\n", b.ptr, b.ptr-1)
 			b.ptr--
 		case '>':
-			log.Debugf("add pointer %d -> %d\n", b.ptr, b.ptr+1)
+			// log.Debugf("add pointer %d -> %d\n", b.ptr, b.ptr+1)
 			b.ptr++
 		case '+':
 			v := b.stack[b.ptr]
@@ -74,19 +109,69 @@ func (b *fucked) process(code string) {
 		case '[':
 			// take note of location in code to jump back to
 			log.Debug("loop start")
-			b.looplines = append(b.looplines, i)
-		case ']':
-			if b.stack[b.ptr] == DONE {
-				log.Debug("loop done")
-				b.looplines = b.looplines[:len(b.looplines)-1]
-			} else {
-				i = b.looplines[len(b.looplines)-1]
-				log.Debugf("loop to %d\n", i)
-				continue
+			if !b.hasLoop(i) {
+				b.loops = append(b.loops, i)
 			}
+			// b.looplines = append([]int{i}, b.looplines...)
+		case ']':
+			if b.stack[b.ptr] != DONE {
+				i = b.loops[len(b.loops)-1]
+				log.Debugf("loop to %d\n", i)
+				b.loops = b.loops[:len(b.loops)-1]
+			}
+			// 	log.Debugf("loop done, left %d\n", len(b.loops))
+			// b.loops = b.loops[:len(b.loops)-1]
+			// 	if len(b.loops) == 2 {
+			// 		os.Exit(0)
+			// 	}
+			// } else {
+			// }
+		case ',':
+			b.stack[b.ptr] = b.read()
+			fmt.Printf("%+v\n", b.stack)
 		case '.':
 			fmt.Print(string(b.stack[b.ptr]))
 		}
 		i++
 	}
+}
+
+func (b *fucked) clean(dirty string) string {
+	cleaned := ""
+	for _, op := range dirty {
+		switch op {
+		case '<', '>', '+', '-', '[', ']', ',', '.':
+			cleaned += string(op)
+		}
+	}
+	return cleaned
+}
+
+func (b *fucked) String() string {
+	output := "["
+	for i, p := range b.stack {
+		if i == b.ptr {
+			output += log.BoldYellow
+		}
+		output += fmt.Sprintf(" %d ", p)
+		if i == b.ptr {
+			output += log.Reset
+		}
+	}
+	return output
+}
+
+func (b *fucked) hasLoop(i int) bool {
+	for _, l := range b.loops {
+		if i == l {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *fucked) read() byte {
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	return []byte(input)[0]
 }
